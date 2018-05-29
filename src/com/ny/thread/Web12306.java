2569f966e2f52ed9568de4f3937280ccc9a75705
聂野 <2691439293@qq.com>
2018-05-29 10:37:07 +0800

    Update Web12306.java

--- src/com/ny/thread/Web12306.java
+++ src/com/ny/thread/Web12306.java
@@ -19,6 +19,7 @@ public class Web12306 implements  Runnable {
          Web12306 real=new Web12306();
          Web12306 real1=new Web12306();
          String str= new String();
+          System.out.println("github直接新增");
          Thread t1=new Thread(real,"t1");
         Thread t2=new Thread(real,"t2");
         Thread t3=new Thread(real,"t3");
